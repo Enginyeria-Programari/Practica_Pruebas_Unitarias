35ffd0acb256915f4c8e7bd015ffaeafca02d264
samplecode17 <ibrahim_mou17@hotmail.com>
2023-12-19 13:43:43 +0100

    biometric Exception fixed from fail

--- src/Controller/Exceptions/BiometricVerificationFailedException.java
+++ src/Controller/Exceptions/BiometricVerificationFailedException.java
@@ -1,2 +1,7 @@
-package Controller.Exceptions;public class BiometricVerificationFailedException {
+package Controller.Exceptions;
+
+public class BiometricVerificationFailedException extends Exception {
+    public BiometricVerificationFailedException(String msg) {
+        super(msg);
+    }
 }
